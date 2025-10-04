120d5a31ae57b2883de29c9035d37cc5b7d24270
da-pic <dapic2411@gmail.com>
2025-10-04 11:32:14 +0700

    ...

---
+++ website/enrollment.py
@@ -0,0 +1,55 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .models import conn
+
+enrollment = Blueprint('enrollment', __name__)
+
+@enrollment.route('/enrollment/<student_id>')
+def enrollment_page(student_id):
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("""
+        SELECT
+            c.course_id,
+            c.course_name,
+            cc.volume,
+            cc.course_class_id,
+            l.lecturer_name
+        FROM course_class cc
+        JOIN course c ON cc.course_id = c.course_id
+        JOIN lecturer l ON cc.lecturer_id = l.lecturer_id
+    """)
+    course_class = cursor.fetchall()
+    cursor.close()
+
+    return render_template("enrollment.html", student_id=student_id, course_class=course_class)
+
+
+@enrollment.route('/enrollment/<student_id>/register', methods=['POST'])
+def register_courses(student_id):
+    conn.ping(reconnect=True)  # đảm bảo kết nối MySQL còn sống
+    selected = request.form.getlist('selected_courses')
+    print(f"➡️ Bắt đầu xử lý đăng ký cho: {student_id}")
+    print(f"🧩 Các môn được chọn: {selected}")
+
+    has_error = False
+    cursor = conn.cursor()
+
+    try:
+        for cc_id in selected:
+            print(f"🔹 Đang thêm lớp {cc_id}...")
+            cursor.execute("""
+                INSERT INTO enrollment (student_id, course_class_id)
+                VALUES (%s, %s)
+            """, (student_id, cc_id))
+        conn.commit()
+        flash("✅ Đăng ký thành công!", "success")
+
+    except Exception as e:
+        conn.rollback()
+        has_error = True
+        print(f"⚠️ Lỗi SQL: {e}")
+        flash(f"⚠️ bạn đã đăng ký {cc_id}", "error")
+
+    finally:
+        cursor.close()
+
+    return redirect(url_for('enrollment.enrollment_page', student_id=student_id))
--- website/init.py
+++ website/init.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from .auth import auth
 from .views import views
+from .enrollment import enrollment
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = 'quang anh dep trai vocolo'
     app.register_blueprint(auth)
     app.register_blueprint(views)
+    app.register_blueprint(enrollment, url_prefix='/')
     return app
